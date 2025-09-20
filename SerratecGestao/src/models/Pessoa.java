1c8078e2fccf33fe7e339c4e9a0df63904962a71
Viviane Andrade <vivianeandrrade@gmail.com>
2025-09-19 23:11:42 -0300

    Classes Pessoa, Funcionario e Calculo IR

---
+++ SerratecGestao/src/models/Pessoa.java
@@ -0,0 +1,57 @@
+package models;
+
+import java.time.LocalDate;
+
+public abstract class Pessoa {
+    private Integer id;
+    private String nome;
+    private String cpf;
+    private LocalDate dataNascimento;
+
+    public Pessoa() {
+    }
+
+    public Pessoa(Integer id, String nome, String cpf, LocalDate dataNascimento) {
+        this.id = id;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.dataNascimento = dataNascimento;
+    }
+
+    // getters / setters
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public int getIdade() {
+        return java.time.Period.between(this.dataNascimento, LocalDate.now()).getYears();
+    }
+}
